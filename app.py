9a4f0178ec243b1cc600b65acb693b330b6096e6
Vipparla Veeranjaneyulu <veeranjaneyuluvipparla.2109@gmail.com>
2025-06-25 14:48:25 +0530

    Prepare for Render deployment

--- app.py
+++ app.py
@@ -596,4 +596,5 @@ def payment():
     
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render provides PORT
+    app.run(host='0.0.0.0', port=port)
